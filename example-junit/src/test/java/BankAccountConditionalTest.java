a47ed6633dedb7981f719ca21fef72e204ba7ed5
Joyce <joyce.simkin@gmail.com>
2024-10-14 20:40:45 -0600

    changes for BankAccountConditionalTest

--- example-junit/src/test/java/BankAccountConditionalTest.java
+++ example-junit/src/test/java/BankAccountConditionalTest.java
@@ -1,17 +1,29 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.*;
+
+
 public class BankAccountConditionalTest {
 
-    public void testMac() {
+    @Test
+    @EnabledOnOs({OS.MAC}) //org.junit.jupiter.api.condition.OS
+    public void testMac() { //Ignored because I'm running test on Windows
 
     }
 
+    @Test
+    @EnabledOnOs({OS.WINDOWS})
     public void testWindows() {
 
     }
 
+    @Test
+    @EnabledOnJre({JRE.JAVA_16}) //ignored because I'm running JRE 19.0.1
     public void testJRE() {
 
     }
 
+    @Test
+    @DisabledOnJre({JRE.JAVA_16})
     public void testNoJRE16() {
         
     }
